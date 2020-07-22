266987713654095f5b3b9d13db6431fe0c9ddcc6
Mila <mashtari1991@gmail.com>
2020-07-22 18:50:21 -0400

    number of moves and time counter added

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 const gameApp={}
 gameApp.parentClass=[]
 gameApp.i=0
+gameApp.k=0
 gameApp.choice=[]
 gameApp.init=function(){
     gameApp.randOrder()
     gameApp.click()
+    gameApp.timer()
 }
 
 gameApp.randOrder=function(){
 $('li').css('order',function(){
     return Math.floor(Math.random()*16)
 })}
+gameApp.timer=function(){
+    $('.startGame').on('click',function(){
+        let start=0
+        setInterval(function() {
+        start++
+        $('.timer span').text(start)
+    }, 1000);
+    }) 
+}
 gameApp.click=function(){
-$('div').on('click', function(){
-
-    gameApp.i++ // counter to count number of clicks 
-    // console.log(gameApp.i)
-    gameApp.choice[gameApp.i]=$(this) //
-   $(this).removeClass('hidden')
-    $(this).addClass('unhidden')
-    gameApp.parentClass[gameApp.i] = $(this).parent()['0']['className']
-    if(gameApp.i==2){
-   
-        setTimeout(gameApp.hiddenUnhidden, 1000,gameApp.i)
+    let movesCounter=0;
+        $('div').on('click', function(){
+            if(gameApp.i<2){
+            movesCounter++
+            $('.movesCounter span').text(movesCounter)
+            gameApp.i++ // counter to count number of clicks 
+            gameApp.choice[gameApp.i]=$(this) //storing the clicked item in the choice array
+            $(this).removeClass('hidden')
+            $(this).addClass('unhidden')
+            gameApp.parentClass[gameApp.i] = $(this).parent()['0']['className']
+            if(gameApp.i==2){
+                setTimeout(gameApp.hiddenUnhidden, 1000,gameApp.i)
+            }
     }
-})
+    })
 }
 gameApp.hiddenUnhidden=function(j){
-    
+    // if the parentClass of the two clicked items are the same they remain unhidden else they will hide again
     if (gameApp.parentClass[j - 1] == gameApp.parentClass[j]) {
         gameApp.i=0;
         gameApp.winAlert()
